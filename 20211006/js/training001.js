a22eb97a83de01c4af0feb65435ac4085bb5d6b4
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-10-06 14:21:25 +0900

    Add files via upload

---
+++ 20211006/js/training001.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(".문어").on("click",function(){
+        $("h1").hide();
+    });
+
+   $(".오징어").on("click",function(){
+        $("h1").show();
+    });
+    $(".나는버튼").on("click",function(){
+        $("figure").toggleclass("on")
+    });
+    
+});
