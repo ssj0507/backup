cec49eff3f2fadfe88d9b48400a33218b3c80221
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-10-20 13:59:35 +0900

    Add files via upload

--- 20211019/js/traning008.js
+++ 20211019/js/traning008.js
@@ -7,7 +7,7 @@ $(function(){
     
     $('.main_slider').slick({
         arrows:false,
-        dots:true,
+        //dots:true,
         autoplay:true,
         pauseOnHover:false,
     });
@@ -16,6 +16,14 @@ $(function(){
     
     $('.main_slider').on('afterChange', function(e,s,c){
         $('.main_slider figure').eq(c+1).addClass('on').siblings().removeClass('on');
+    });
+
+
+    $('#con04 i.xi-pause').on('click', function(){
+        $('#mv01').trigger('pause')
+    });
+    $('#con04 i.xi-play').on('click', function(){
+        $('#mv01').trigger('play')
     });
     //----------------------------------------------
     });
