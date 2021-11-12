27a701349ffe09b7c39f0eedcbf92594e304ae9c
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-11-12 11:32:14 +0900

    Add files via upload

---
+++ doc/animation01.js
@@ -0,0 +1,20 @@
+$(function(){
+//----------------------------------
+
+$('.btn li').on('click', slidingbal);
+function slidingbal() {
+    let idx=$(this).index();
+    console.log(idx);
+    $('.case').animate({top:-450*idx}, 1000, "easeOutBounce")
+
+}
+
+
+
+
+
+
+
+
+//-------------------------------------
+});
