e8267e414753e770252e16c5bc36784bc1407a3c
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-11-23 12:49:05 +0900

    Add files via upload

---
+++ trn01/layout.js
@@ -0,0 +1,27 @@
+$(function(){
+//---------------------------------------------------
+$(window).on('scroll', function(){
+    var sct=$(window).scrollTop(); //var let const 
+    sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on'); // 삼항조건식
+    // if(sct > 0) {
+    //     $('.header').addClass('on')
+    // } else {
+    //     $('.header').removeClass('on')  
+    // }
+});
+
+$('.main_slider').slick({
+    arrows:false,
+    fade:true,
+    dots:true,
+    autoplay:true,
+    pauseOnHover:false,
+    pauseOnFocus:false,
+});
+
+
+
+
+
+//-------------------------
+});
