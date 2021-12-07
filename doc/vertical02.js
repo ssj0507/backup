a9b3455f1354ca85785990636601c1dbaa72e208
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-12-07 10:39:58 +0900

    Add files via upload

--- doc/vertical02.js
+++ doc/vertical02.js
@@ -5,7 +5,7 @@ $(function(){
         autplay:true,
         dots:true,
         prevArrow:'<div class="prev"><i class="xi-arrow-left"></i></div>',
-        nextArrow:'<div class="next"><i class="xi-arrow-right"></i></div>'
+        nextArrow:'<div class="next"><i class="xi-arrow-right"></i></div>',
      
 
     });
@@ -16,5 +16,23 @@ $(function(){
         $('.main_visual figure').eq(c+1).addClass('on').siblings().removeClass('on');
     });
 
-    
-})
+   
+    $('.mopen').on('click', function(){
+        $('#header').toggleClass('on');
+    });
+
+    $('nav>ul>li>a').on('click', function(){
+        if($('#header').hasClass('on')){
+            $(this).next().slideToggle();
+            $(this).parent().siblings().find('.submenu').slideUp();
+        }
+      
+    });
+    $(window).on('resize', function(){
+        if($(window).width() > 768){
+            $('#header').removeClass('on');
+            $('.submenu').removeAttr('style');
+        }
+    });
+
+});
