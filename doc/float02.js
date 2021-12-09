739fd4bec7849b3406494f5993d8a1c7e2070f1f
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-12-09 12:49:23 +0900

    Add files via upload

---
+++ doc/float02.js
@@ -0,0 +1,15 @@
+$(function(){
+    //--------------------------
+    $('.main_slider').slick({
+        arrows:false,
+    });
+   
+    //window.onload=function(){
+
+    //}
+
+    //window.addEventListener('DOMCOntentLoaded', function(){
+
+    //})
+//--------------------------
+});
