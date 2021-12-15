8145feb859f7a16942991e4c0790356da63b0160
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2021-12-15 12:33:58 +0900

    Add files via upload

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -8,10 +8,51 @@ $(function(){
                 $('.section').eq(destination.index).addClass("on").siblings().removeClass('on');
                 $('.navbar>li').eq(destination.index).addClass("on").siblings().removeClass('on');
                 $('.now').text(txt);
-                destination.index==0 ? $('.header').fadeIn() : $('.header').fadeOut();
-                destination.index==0 ? $('.wheel').fadeIn() : $('.wheel').fadeOut();
+                destination.index===0 ? $('.header').fadeIn() : $('.header').fadeOut();
+                destination.index===0 ? $('.wheel').fadeIn() : $('.wheel').fadeOut();
             },
         });
+    
+        $('.b_slider').slick({
+            arrows:false,
+            asNavFor:'.b_slider',
+            autoplay:true,
+        });
+    
+        $('.brand_name li').on('click', function(){
+            $(this).addClass('on').siblings().removeClass('on');
+            let idx=$(this).index();
+            $('.b_slider').slick('slickGoTo',idx);
+        });
+
+
+        //포플만들때 사용할때//
+        
+        $('.header>a').on('click', function(){
+            $(this).toggleClass('on');
+            $('.all_menu').slideToggle();
+            if($('.header>a').hasClass('on')) {
+                $.fn.fullpage.setAllowScrolling(false);
+            }else{
+                $.fn.fullpage.setAllowScrolling(true);
+            }
+        });
+    
+    
+        $('.all_menu a').on('click', function(){
+            $('.all_menu').slideUp();
+            $.fn.fullpage.setAllowScrolling(true);
+            $('.header>a').removeClass('on');
+        });
+
+     
+
+        
+        
+
+        
+    
+    
     
     
     
