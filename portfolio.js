b45ff4d68df45d9fcdacce8d785882d72d8049f8
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2022-01-10 17:06:48 +0900

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,40 @@
+$(function(){
+    //-----------------------------------------------------
+    
+    let sc=$('.section');
+    let sideBar=$('nav li');
+    $('#main').fullpage({
+        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'profile'],
+        afterLoad:function(origin, destination, direction){
+            let idx=destination.index;
+            sc.eq(idx).addClass('on').siblings().removeClass('on');
+            sideBar.eq(idx).addClass('on').siblings().removeClass('on');
+        },
+    });
+    
+    
+    //WEB is My MONEY
+    new Typed('.slogan_txt',{
+        strings: ['WEB is My MONEY', 'gggg baby'],
+        typeSpeed:100,
+    });
+    
+    $('.cover_btn').on('click', function(){
+        $("#cover").slideToggle(); //fadeToggle
+    });
+    
+    
+    let cloneMenu = $('nav>ul').clone();
+    $('#cover').append(cloneMenu);
+    
+    $('#cover a').on('click', function(){
+        $('#cover').slideUp(); 
+    });
+    
+    $('#cover').on('scroll wheel touchmove', function(){
+        return false;
+    });
+    
+    
+    //-----------------------------------------------------
+    })
