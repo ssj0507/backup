3c0800a027c81a626e567df9c9096d61caa9e42b
ssj0507 <91935304+ssj0507@users.noreply.github.com>
2022-03-09 09:51:01 +0900

    Add files via upload

---
+++ mobile/trn01/js/layout.js
@@ -0,0 +1,89 @@
+$(function () {
+  //----------------------------------------------------
+  $("#main").fullpage({
+    anchors: ["sc02", "sc03", "sc04", "sc05", "sc06"],
+    //navigation: true,
+    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+    afterLoad: function (origin, destination, direction) {
+      var idx = destination.index;
+      console.log(idx); // 0,1,2....
+      $(".section").eq(idx).addClass("on").siblings().removeClass("on");
+    },
+  });
+  
+  //메인사진 슬라이드
+  $(".l_slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+  });
+
+  //메인사진 슬라이드
+  $(".main_slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    fade: true,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    speed: 2000,
+  });
+
+  //신랑측 팝업
+  $(".btn_m").on("click", function () {
+    $(".tel_m").show("on");
+  });
+  $(".tel_m > div > i").on("click", function () {
+    $(".tel_m").hide("on");
+  });
+  //신부측 팝업
+  $(".btn_w").on("click", function () {
+    $(".tel_w").show("on");
+  });
+  $(".tel_w > div > i").on("click", function () {
+    $(".tel_w").hide("on");
+  });
+  //계좌번호 팝업
+  $(".num").on("click", function () {
+    $(".bank").show("on");
+  });
+  $(".bank>div>i").on("click", function () {
+    $(".bank").hide("on");
+  });
+
+  //탭 메뉴 - 롤링 페이퍼
+
+  $(".tab_menu>li").on("click", function () {
+    var $this = $(this); //자기 자신
+    var idx = $(this).index(); //자기 자신의 번호 0 1 2
+    //console.log($this,idx);
+    //$('.tab_content>div').eq(idx).show().siblings().hide();
+    $(".tab_content>div").eq(idx).addClass("on").siblings().removeClass("on");
+    $this.addClass("on").siblings().removeClass("on");
+  });
+
+  //-------------------------------------------------------
+});
+  
+
+//계좌번호
+function copy_to_clipboard() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("Copy");
+  alert("복사되었습니다, 감사합니다.");
+}
+function copy_to_clipboard2() {
+  var copyText = document.getElementById("myInput2");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("Copy");
+  alert("복사되었습니다, 감사합니다.");
+}
+// var elem = document.querySelector(".m-p-g");
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var gallery = new MaterialPhotoGallery(elem);
+// });
